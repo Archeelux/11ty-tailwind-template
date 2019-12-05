17ea5c77e75cbeb34cdc1cecd421466b40c947ff
Archeelux <archeelux@gmail.com>
2019-12-05 16:02:19 +0000

    init �

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("precss"),
+    require("postcss-preset-env"),
+    require("tailwindcss"),
+    require("autoprefixer")
+  ]
+};
